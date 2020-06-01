c58a030a69620a73487dba41ea430675e0a764b1
Duarte Alves <Duarte's MBP>
2020-06-01 15:21:19 +0100

    Grcp Server Impl refactored

--- src/server/grpc_service.py
+++ src/server/grpc_service.py
@@ -1,7 +1,7 @@
 from annotations.profiling import profile
 from contract.service_pb2 import PredictRequest, PredictResponse, StreamPredictResponse, Prediction, Correspondence
 from contract.service_pb2_grpc import PredictionServiceServicer
-from server.service import Service
+from model.service import Service
 
 
 class PredictionServerImpl(PredictionServiceServicer):
@@ -11,12 +11,7 @@ class PredictionServerImpl(PredictionServiceServicer):
 
     @profile
     def predict(self, request: PredictRequest, context):
-        image_bytes = request.image_data
-        all_categories = request.all_categories
-        all_attributes = request.all_attributes
-        categories_results, attributes_results = self.__service.predict(image_bytes, all_categories, all_attributes)
-        categories_results = map(lambda x: Correspondence(label=x[0], value=x[1]), categories_results)
-        attributes_results = map(lambda x: Correspondence(label=x[0], value=x[1]), attributes_results)
+        categories_results, attributes_results = self.__process_single_predict(request)
         return PredictResponse(predicted_categories=categories_results,
                                predicted_attributes=attributes_results)
 
@@ -26,10 +21,14 @@ class PredictionServerImpl(PredictionServiceServicer):
         return StreamPredictResponse(predictions=predictions)
 
     def __process_single_request(self, request):
+        categories_results, attributes_results = self.__process_single_predict(request)
+        return Prediction(predicted_categories=categories_results, predicted_attributes=attributes_results)
+
+    def __process_single_predict(self, request):
         image_bytes = request.image_data
         all_categories = request.all_categories
         all_attributes = request.all_attributes
         categories_results, attributes_results = self.__service.predict(image_bytes, all_categories, all_attributes)
         categories_results = map(lambda x: Correspondence(label=x[0], value=x[1]), categories_results)
         attributes_results = map(lambda x: Correspondence(label=x[0], value=x[1]), attributes_results)
-        return Prediction(predicted_categories=categories_results, predicted_attributes=attributes_results)
+        return categories_results, attributes_results
